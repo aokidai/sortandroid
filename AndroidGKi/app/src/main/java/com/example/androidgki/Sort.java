cee3bab2f00d01a489c638737eef9a6c7e28ffff
aokidai <aokidaisuke91@gmail.com>
2021-02-25 13:16:45 +0700

    clearall

--- AndroidGKi/app/src/main/java/com/example/androidgki/Sort.java
+++
@@ -1,7 +0,0 @@
-package com.example.androidgki;
-
-import java.util.ArrayList;
-
-public interface Sort {
-    public void sort (int[] arr, int n, String[] Chitiet);
-}
--- sortandroid/app/src/main/java/com/example/sortandroid/Sort.java
+++
@@ -1,6 +0,0 @@
-package com.example.sortandroid;
-
-public interface Sort {
-    public void sort (int[] arr, int n, String[] Chitiet);
-}
-
